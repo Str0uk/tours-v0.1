71f34a7f8fc4c0973d4005c32ad0c2b359e514bf
Misha <strouk7@yandex.ru>
2021-06-08 20:41:40 +0300

    First commit

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -4,9 +4,9 @@ from tours.views import MainView, DepartureView, TourView
 
 urlpatterns = [
     path('main/', MainView.as_view()),
-    path('/', MainView.as_view()),
-    path('/departure/', DepartureView.as_view()),  #'main/departure/<str:departure>'
-    path('/tour/<int:id>', TourView.as_view()),
-    path('/tour/', TourView.as_view()),
+    path('main/', MainView.as_view()),
+    path('main/departure/', DepartureView.as_view()),  #'main/departure/<str:departure>'
+    path('main/tour/<int:id>', TourView.as_view()),
+    path('main/tour/', TourView.as_view()),
     path('admin/', admin.site.urls),
 ]
